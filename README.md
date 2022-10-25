522be833db95b1dcef17a6061a7693ce37831080
Lugixion <lopezLD1708@gmail.com>
2022-10-25 18:10:36 +0100

    prueba con waitkey

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Ajedrez con las manos
 Crear un ajedrez que se puede jugar con gestos de las manos a través de Python con los módulos OpenCV, Tensorflow y MediaPipe. Un set de instrucciones de movimientos predefinidos detectados por una inteligencia artificial.
 
 **Set de instrucciones**<br>
-· Seleccionar pieza, X(Número, en caso de ser mayor de 5, poner 2 veces un número y se sumará) + Y(Letra, con gestos del ASL simples)
-· Cancelar: Puño cerrado
-· Seleccionar: Signo OK
-· 
+· Seleccionar coordenadas: Y(Número, en caso de ser mayor de 5, poner 2 veces un número y se sumará) + X(Letra, con gestos del ASL simples).
+· Cancelar: Puño cerrado.
+· Seleccionar: Signo OK.
+
 <br><br>
 ### Creación
 · **Entrenamiento con Keras (Tensorflow)**<br>
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ mpDraw = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
 
+ftw = open("data","a")
+
 while True:
     _, frame = cap.read()
 
@@ -31,11 +33,63 @@ while True:
 
             mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
 
+
     cv2.imshow("Output", frame) 
 
-    if cv2.waitKey(1) == ord('q'):
+    k = cv2.waitKey(1)
+
+    if k in ['q','1','2','']:
         break
 
+    if cv2.waitKey(1)  == ord('1'):
+        landmarks.append(1)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('2'):
+        landmarks.append(2)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('3'):
+        landmarks.append(3)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('4'):
+        landmarks.append(4)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('5'):
+        landmarks.append(5)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('a'):
+        landmarks.append(6)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('b'):
+        landmarks.append(7)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('c'):
+        landmarks.append(8)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('d'):
+        landmarks.append(9)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('e'):
+        landmarks.append(10)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('f'):
+        landmarks.append(11)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('g'):
+        landmarks.append(12)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('h'):
+        landmarks.append(13)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('o'):
+        landmarks.append(14)
+        ftw.write(str(landmarks)+"\n")
+    elif cv2.waitKey(1)  == ord('n'):
+        landmarks.append(15)
+        ftw.write(str(landmarks)+"\n")
+
+    
 cap.release()
 
 cv2.destroyAllWindows()
+
+ftw.close()
